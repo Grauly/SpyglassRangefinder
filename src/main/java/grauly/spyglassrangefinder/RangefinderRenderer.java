487b89a9e444f0a773e54808f92277e115d3462b
Grauly <nils.dellemann@web.de>
2022-07-26 21:56:13 +0200

    feat: added functionality

---
+++ src/main/java/grauly/spyglassrangefinder/RangefinderRenderer.java
@@ -0,0 +1,24 @@
+package grauly.spyglassrangefinder;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.OrderedText;
+import net.minecraft.util.hit.HitResult;
+
+public class RangefinderRenderer {
+
+    private static MinecraftClient mc = MinecraftClient.getInstance();
+    public static void onRender(MatrixStack matrices, float tickDelta) {
+        if(mc.player.isUsingSpyglass()) {
+            var result = mc.player.raycast(1024,tickDelta,false);
+            if(!result.getType().equals(HitResult.Type.MISS)) {
+                var width = mc.getWindow().getScaledWidth();
+                var height = mc.getWindow().getScaledHeight();
+                var distance = Math.sqrt(result.squaredDistanceTo(mc.player));
+                var showSting = String.format("%.2f",distance);
+                DrawableHelper.drawCenteredText(matrices,mc.textRenderer,showSting,width/2 + mc.textRenderer.getWidth(showSting)/2 + 10,height/2 - mc.textRenderer.fontHeight/2,-1);
+            }
+        }
+    }
+}
--- src/main/java/grauly/spyglassrangefinder/SpyglassRangefinderClient.java
+++ src/main/java/grauly/spyglassrangefinder/SpyglassRangefinderClient.java
@@ -1,4 +1,12 @@
 package grauly.spyglassrangefinder;
 
-public class SpyglassRangefinderClient {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+
+public class SpyglassRangefinderClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        HudRenderCallback.EVENT.register(RangefinderRenderer::onRender);
+    }
 }
