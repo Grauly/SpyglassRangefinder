790fa60fe9704bf4bd1c7fbdbc1fac82fcb068e8
Grauly <nils.dellemann@web.de>
2022-07-27 11:37:41 +0200

    fix: fixed logger still using "modid"

--- src/main/java/grauly/spyglassrangefinder/SpyglassRangefinder.java
+++ src/main/java/grauly/spyglassrangefinder/SpyglassRangefinder.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 public class SpyglassRangefinder implements ModInitializer {
 
 	public static final String MODID = "spyglassrangefinder";
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
 
 	@Override
 	public void onInitialize() {
