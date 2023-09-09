7dbb3e838564f2cc24adeb2b914ee88f018ec363
Grauly <nils.dellemann@web.de>
2023-09-10 00:05:27 +0200

    chore: updated code

--- src/main/java/grauly/spyglassrangefinder/rendering/RangefinderRenderer.java
+++ src/main/java/grauly/spyglassrangefinder/rendering/RangefinderRenderer.java
@@ -1,15 +1,13 @@
 package grauly.spyglassrangefinder.rendering;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.OrderedText;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.util.hit.HitResult;
 
 public class RangefinderRenderer {
 
     private static MinecraftClient mc = MinecraftClient.getInstance();
-    public static void onRender(MatrixStack matrices, float tickDelta) {
+    public static void onRender(DrawContext context, float tickDelta) {
         if(mc.player.isUsingSpyglass()) {
             var result = mc.player.raycast(1024,tickDelta,false);
             if(!result.getType().equals(HitResult.Type.MISS)) {
@@ -17,7 +15,7 @@ public class RangefinderRenderer {
                 var height = mc.getWindow().getScaledHeight();
                 var distance = Math.sqrt(result.squaredDistanceTo(mc.player));
                 var showSting = String.format("%.2f",distance);
-                DrawableHelper.drawCenteredTextWithShadow(matrices,mc.textRenderer,showSting,width/2 + mc.textRenderer.getWidth(showSting)/2 + 10,height/2 - mc.textRenderer.fontHeight/2,-1);
+                context.drawCenteredTextWithShadow(mc.textRenderer,showSting,width/2 + mc.textRenderer.getWidth(showSting)/2 + 10,height/2 - mc.textRenderer.fontHeight/2,-1);
             }
         }
     }
