03be1d33785a8739729fcd69ca0a93ba889bb29a
Grauly <nils.dellemann@web.de>
2024-08-06 00:32:38 +0200

    feat: rendering fixup

--- src/main/java/grauly/spyglassrangefinder/rendering/RangefinderRenderer.java
+++ src/main/java/grauly/spyglassrangefinder/rendering/RangefinderRenderer.java
@@ -2,14 +2,15 @@ package grauly.spyglassrangefinder.rendering;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.util.hit.HitResult;
 
 public class RangefinderRenderer {
 
     private static MinecraftClient mc = MinecraftClient.getInstance();
-    public static void onRender(DrawContext context, float tickDelta) {
+    public static void onRender(DrawContext context, RenderTickCounter renderTickCounter) {
         if(mc.player.isUsingSpyglass()) {
-            var result = mc.player.raycast(1024,tickDelta,false);
+            var result = mc.player.raycast(1024,renderTickCounter.getTickDelta(false),false);
             if(!result.getType().equals(HitResult.Type.MISS)) {
                 var width = mc.getWindow().getScaledWidth();
                 var height = mc.getWindow().getScaledHeight();
