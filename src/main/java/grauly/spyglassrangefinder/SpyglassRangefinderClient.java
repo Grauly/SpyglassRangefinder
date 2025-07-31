5eac01bfdf800b1273f46055ac2a04efbaf9b4df
Grauly <nils.dellemann@web.de>
2025-07-31 16:03:22 +0200

    fix: update to HudElementRegistry

--- src/main/java/grauly/spyglassrangefinder/SpyglassRangefinderClient.java
+++ src/main/java/grauly/spyglassrangefinder/SpyglassRangefinderClient.java
@@ -2,9 +2,8 @@ package grauly.spyglassrangefinder;
 
 import grauly.spyglassrangefinder.rendering.RangefinderRenderer;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.HudLayerRegistrationCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
 import net.minecraft.util.Identifier;
 
 public class SpyglassRangefinderClient implements ClientModInitializer {
@@ -13,8 +12,6 @@ public class SpyglassRangefinderClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        HudLayerRegistrationCallback.EVENT.register(layeredDrawerWrapper -> {
-            layeredDrawerWrapper.attachLayerAfter(IdentifiedLayer.MISC_OVERLAYS, RANGEFINDER_LAYER, RangefinderRenderer::onRender);
-        });
+        HudElementRegistry.attachElementAfter(VanillaHudElements.MISC_OVERLAYS, RANGEFINDER_LAYER, RangefinderRenderer::onRender);
     }
 }
