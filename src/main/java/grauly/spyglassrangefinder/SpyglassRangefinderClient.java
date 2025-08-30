e33f265691be346a33c033403afb95b963ddb17c
Grauly <nils.dellemann@web.de>
2025-08-30 12:49:06 +0200

    feat: add teleport hotkey

--- src/main/java/grauly/spyglassrangefinder/SpyglassRangefinderClient.java
+++ src/main/java/grauly/spyglassrangefinder/SpyglassRangefinderClient.java
@@ -2,16 +2,28 @@ package grauly.spyglassrangefinder;
 
 import grauly.spyglassrangefinder.rendering.RangefinderRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
 import net.minecraft.util.Identifier;
+import org.lwjgl.glfw.GLFW;
 
 public class SpyglassRangefinderClient implements ClientModInitializer {
 
     public static final Identifier RANGEFINDER_LAYER = Identifier.of(SpyglassRangefinder.MODID, "rangefinder");
+    public static final KeyBinding tpKeyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+            "key." + SpyglassRangefinder.MODID + "teleport",
+            InputUtil.Type.KEYSYM,
+            GLFW.GLFW_KEY_GRAVE_ACCENT,
+            "category." + SpyglassRangefinder.MODID + ".category"
+    ));
 
     @Override
     public void onInitializeClient() {
         HudElementRegistry.attachElementAfter(VanillaHudElements.MISC_OVERLAYS, RANGEFINDER_LAYER, RangefinderRenderer::onRender);
+        ClientTickEvents.END_CLIENT_TICK.register(TeleportTrigger::tick);
     }
 }
---
+++ src/main/java/grauly/spyglassrangefinder/TeleportTrigger.java
@@ -0,0 +1,18 @@
+package grauly.spyglassrangefinder;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.hit.HitResult;
+
+public class TeleportTrigger {
+    private static int cooldown = 0;
+    public static void tick(MinecraftClient minecraftClient) {
+        cooldown -= 1;
+        if (cooldown > 0) return;
+        if (!SpyglassRangefinderClient.tpKeyBind.wasPressed()) return;
+        if (!minecraftClient.player.isUsingSpyglass()) return;
+        var result = minecraftClient.player.raycast(1024, minecraftClient.getRenderTickCounter().getTickProgress(false),false);
+        if (result.getType() != HitResult.Type.BLOCK) return;
+        minecraftClient.getNetworkHandler().sendChatCommand("tp " + result.getPos().x + " " + result.getPos().y + " " + result.getPos().z);
+        cooldown = 10;
+    }
+}
